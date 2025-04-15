97f27bd1b7e4d982482266ec419bacfa5be6a028
Elusiyan Mathew Eluwole <156443122+eeluwole1@users.noreply.github.com>
2025-04-14 20:41:54 -0500

    Create lambda_function.py

---
+++ delete_inventory_item/lambda_function.py
@@ -0,0 +1,13 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('Inventory')
+
+def lambda_handler(event, context):
+    item_id = event['pathParameters']['id']
+    table.delete_item(Key={'item_id': item_id})
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'message': 'Item deleted'})
+    }
